126181170d43775b842e7d7c2d8815afc1a754fb
Rikard Holm <rikard.holm@avanzabank.se>
2014-05-12 12:36:15 +0200

    Basic scaffolding for Oracle repositories.

---
+++ insurance-domain/src/test/java/rikardholm/insurance/infrastructure/oracle/OracleInsuranceRepositoryTest.java
@@ -0,0 +1,31 @@
+package rikardholm.insurance.infrastructure.oracle;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import rikardholm.insurance.domain.AbstractInsuranceRepositoryTest;
+import rikardholm.insurance.domain.CustomerRepository;
+import rikardholm.insurance.domain.InsuranceRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:META-INF/insurance/spring/mybatis-context-oracle.xml","classpath*:test/spring/datasource-oracle.xml"})
+@Ignore
+public class OracleInsuranceRepositoryTest extends AbstractInsuranceRepositoryTest{
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private InsuranceRepository insuranceRepository;
+
+	@Override
+	protected CustomerRepository getCustomerRepository() {
+		return customerRepository;
+	}
+
+	@Override
+	protected InsuranceRepository getInstance() {
+		return insuranceRepository;
+	}
+}
