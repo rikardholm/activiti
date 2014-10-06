29923134c0e2dd81deb88cdca553f95475b9b171
rikard <rikardholm@gmail.com>
2014-10-07 01:40:16 +0200

    Message:  - Null-checks  - id -> uuid for consistency  - equals and hashcode  - toString
     Repository: Testing order

--- insurance-domain/src/test/java/rikardholm/insurance/infrastructure/h2/H2MessageRepositoryTest.java
+++ insurance-domain/src/test/java/rikardholm/insurance/infrastructure/h2/H2MessageRepositoryTest.java
@@ -1,7 +1,5 @@
 package rikardholm.insurance.infrastructure.h2;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import rikardholm.insurance.application.messaging.Message;
@@ -11,29 +9,63 @@ import rikardholm.insurance.common.test.database.InMemoryDatabaseRule;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.SortedSet;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasSize;
 
 public class H2MessageRepositoryTest {
 
     @Rule
     public InMemoryDatabaseRule database = new InMemoryDatabaseRule();
+    private MessageRepository2 messageRepository = new H2MessageRepository(database.dataSource);
 
     @Test
     public void appends_and_reads_message() throws Exception {
-        MessageRepository2 messageRepository2 = new H2MessageRepository(database.dataSource);
 
         Instant instant = Instant.parse("2014-10-04T23:00:00Z");
         Message message = MessageBuilder
                 .aMessage()
                 .receivedAt(instant)
+                .type("test")
+                .payload("{\"test\": 123}")
                 .build();
-        messageRepository2.append(message);
+        messageRepository.append(message);
 
-        List<Message> messages = messageRepository2.receivedAfter(instant);
+        List<Message> messages = messageRepository.receivedAfter(instant);
 
         assertThat(messages, hasSize(1));
+    }
+
+    @Test
+    public void returns_messages_in_chronological_order() throws Exception {
+        Message a = MessageBuilder
+                .aMessage()
+                .receivedAt(Instant.parse("2014-10-03T23:00:00Z"))
+                .type("a")
+                .payload("blabal")
+                .build();
+
+        Message b  = MessageBuilder
+                .aMessage()
+                .receivedAt(Instant.parse("2014-10-04T23:00:00Z"))
+                .type("b")
+                .payload("asdf")
+                .build();
+
+        Message c = MessageBuilder
+                .aMessage()
+                .receivedAt(Instant.parse("2014-10-04T23:00:01Z"))
+                .type("c")
+                .payload("dfgfg")
+                .build();
+
+        messageRepository.append(b);
+        messageRepository.append(c);
+        messageRepository.append(a);
+
+        List<Message> messages = messageRepository.receivedAfter(Instant.parse("2014-10-03T22:00:00Z"));
+
+        assertThat(messages, contains(a,b,c));
     }
 }
