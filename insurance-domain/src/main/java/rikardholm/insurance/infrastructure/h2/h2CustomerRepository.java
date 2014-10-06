a4856cab2b3547b106d4d1025a80065d2eb028c1
rikard <rikardholm@gmail.com>
2014-10-07 00:10:48 +0200

    Created H2 Repositories Created H2 SQL scripts Created H2 in-memory DataSource Moved oracle migrations Moved hamcrest matchers to "hamcrest" package

--- insurance-domain/src/main/java/rikardholm/insurance/infrastructure/h2/h2CustomerRepository.java
+++ insurance-domain/src/main/java/rikardholm/insurance/infrastructure/h2/h2CustomerRepository.java
@@ -1,7 +1,43 @@
 package rikardholm.insurance.infrastructure.h2;
 
-/**
- * Created by rikard on 2014-09-24.
- */
-public class h2CustomerRepository {
+import com.google.common.base.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rikardholm.insurance.domain.customer.Customer;
+import rikardholm.insurance.domain.customer.CustomerRepository;
+import rikardholm.insurance.domain.customer.PersonalIdentifier;
+
+public class h2CustomerRepository implements CustomerRepository {
+    private static final Logger log = LoggerFactory.getLogger(h2CustomerRepository.class);
+    private final CustomerMapper customerMapper;
+
+    public h2CustomerRepository(CustomerMapper customerMapper) {
+        this.customerMapper = customerMapper;
+    }
+
+    @Override
+    public Optional<? extends Customer> findBy(PersonalIdentifier personalIdentifier) {
+        return Optional.fromNullable(customerMapper.findByPersonalIdentifier(personalIdentifier));
+    }
+
+    @Override
+    public void save(Customer instance) {
+        checkCustomerDoesNotExist(instance);
+        log.info("Saving customer {}", instance.getPersonalIdentifier());
+        customerMapper.insert(instance);
+    }
+
+    private void checkCustomerDoesNotExist(Customer instance) {
+        PersonalIdentifier personalIdentifier = instance.getPersonalIdentifier();
+        Optional<? extends Customer> existing = Optional.fromNullable(customerMapper.findByPersonalIdentifier(personalIdentifier));
+
+        if (existing.isPresent()) {
+            throw new IllegalArgumentException("Personal Identifier already exists: " + personalIdentifier);
+        }
+    }
+
+    @Override
+    public void delete(Customer instance) {
+        customerMapper.delete(instance);
+    }
 }
