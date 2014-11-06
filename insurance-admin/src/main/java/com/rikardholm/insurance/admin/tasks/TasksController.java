98f943577e489117dbc2d1fe02ec47250240d3cd
Rikard Holm <rikardholm@gmail.com>
2014-11-06 23:31:38 +0100

    Querying tasks and processes

--- insurance-admin/src/main/java/com/rikardholm/insurance/admin/tasks/TasksController.java
+++ insurance-admin/src/main/java/com/rikardholm/insurance/admin/tasks/TasksController.java
@@ -1,8 +1,10 @@
 package com.rikardholm.insurance.admin.tasks;
 
 import org.activiti.engine.FormService;
+import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import rikardholm.insurance.infrastructure.fake.FakeSparService;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -28,12 +32,24 @@ public class TasksController {
     private FormService formService;
     @Autowired
     private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
+
+    @Autowired
+    private FakeSparService fakeSparService;
 
     @RequestMapping(value = "/register",method = POST)
-    public void register(@RequestParam String personalIdentifier, @RequestParam String address) {
+    public void register(@RequestParam(required = false) String personalIdentifier, @RequestParam(required = false) String address) {
+        fakeSparService.makeUnavailable();
+
         Map<String, String> properties = new HashMap<>();
-        properties.put("personalIdentifier", personalIdentifier);
-        properties.put("address", address);
+        if (isNotBlank(personalIdentifier)) {
+            properties.put("personalIdentifier", personalIdentifier);
+        }
+
+        if (isNotBlank(address)) {
+            properties.put("address", address);
+        }
 
         ProcessDefinition processDefinition = repositoryService
                 .createProcessDefinitionQuery()
@@ -54,6 +70,10 @@ public class TasksController {
                 .list();
 
         model.addAttribute("processes", processDefinitions);
+
+        List<HistoricTaskInstance> historicTasks = historyService.createHistoricTaskInstanceQuery().list();
+
+        model.addAttribute("historicTasks", historicTasks);
     }
 
 }
